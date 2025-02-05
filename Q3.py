6e3524d17bd8c4869d3f337315c49937199c3e1d
Vaus <102696463+V3nti@users.noreply.github.com>
2025-02-05 20:35:58 +0200

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,14 @@
+# Q3
+array = []
+is_true = True
+
+while is_true:
+    new_word = input("Please enter a new word: ")
+
+    for current_word in array:
+        if new_word == current_word:
+            is_true = False
+    else:
+        array.append(new_word)
+
+    print(array)
